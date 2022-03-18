3a1c1b7d1aa57190baf6663ff7c3b69a96accec0
AbhishekKargawal <kargawal.abhishek@gmail.com>
2022-03-18 15:01:06 +0000

    Shap example

--- ShapExample.py
+++ ShapExample.py
@@ -1,54 +1,22 @@
-from pprint import pprint
-import lightgbm as lgb
-import matplotlib.pyplot as plt
 import numpy as np
-import pickle
+import pandas as pd  
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
 import shap
-from sklearn.model_selection import train_test_split, StratifiedKFold
-import warnings
 
-X, y = shap.datasets.adult()
-X_display, y_display = shap.datasets.adult(display=True)
 
-# create a train/test split
-random_state = 7
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
-d_train = lgb.Dataset(X_train, label=y_train)
-d_test = lgb.Dataset(X_test, label=y_test)
+boston = load_boston()
+regr = pd.DataFrame(boston.data)
+regr.columns = boston.feature_names
+regr['MEDV'] = boston.target
 
-params = {
-    "max_bin": 512,
-    "learning_rate": 0.05,
-    "boosting_type": "gbdt",
-    "objective": "binary",
-    "metric": "binary_logloss",
-    "num_leaves": 10,
-    "verbose": -1,
-    "min_data": 100,
-    "boost_from_average": True,
-    "random_state": random_state
-}
+X = regr.drop('MEDV', axis = 1)
+Y = regr['MEDV']
 
-model = lgb.train(params, d_train, 10000, valid_sets=[d_test], early_stopping_rounds=50, verbose_eval=1000)
+fit = LinearRegression().fit(X, Y)
 
-explainer = shap.TreeExplainer(model)
-expected_value = explainer.expected_value
-if isinstance(expected_value, list):
-    expected_value = expected_value[1]
-print(f"Explainer expected value: {expected_value}")
+explainer = shap.LinearExplainer(fit, X, feature_dependence = 'independent')
 
-select = range(20)
-features = X_test.iloc[select]
-features_display = X_display.loc[features.index]
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    shap_values = explainer.shap_values(features)[1]
-    shap_interaction_values = explainer.shap_interaction_values(features)
-if isinstance(shap_interaction_values, list):
-    shap_interaction_values = shap_interaction_values[1]
-
-fig = plt.subplots(figsize=(15, 10), dpi=300)
-shap.decision_plot(expected_value, shap_values, features_display, auto_size_plot=False, show=False)
-
-plt.savefig("summary_plot.png")
+shap_values = explainer.shap_values(X)
+shap.summary_plot(shap_values, X, plot_size=[15, 10])
